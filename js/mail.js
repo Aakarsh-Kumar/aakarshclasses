7f5160f68a365a81a0241ffe5329982ee4c14720
Aakarsh Kumar <aakarsh2504@gmail.com>
2021-08-12 21:22:55 +0530

    first commit

--- js/mail.js
+++ js/mail.js
@@ -1,17 +1,20 @@
 /* SmtpJS.com - v3.0.0 */
-var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
-
-
 function sendEmail() {
 	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "kumaraakarsh25@gmail.com",
-	Password : "pass",
-	To : 'aakarsh2504@gmail.com',
-	From : "kumaraakarsh25@gmail.com",
-	Subject : "Test Mail jsj",
-	Body : "Hello"
-	}).then(function (message) {
-        alert("mail sent successfully"+$(message))  
-    });
+		SecureToken : "37c73fcb-f61f-4229-9dcc-fc10ae0f079c",
+		To : "aakarsh2504@gmail.com",
+		From : "kumaraakarsh25@gmail.com",
+		Subject : "Mail from AakarshClasses Website",
+		Body : "Name: "+document.getElementById('name').value+"\nEmail: "+document.getElementById('email').value+"\nMessage: "+document.getElementById('message').value
+	}).then(
+	  () => alert('Your message have been sent. \n Please refer your given Email for reply.')
+	);
 }
+
+// kumaraakarsh25@gmail.com
+// 6D7074697D4A45E99FBE11A438A1862D772A
+// 2525
+// smtp.elasticemail.com
+
+// Secure Token: 271d7923-7bbf-44bf-a239-262fcd89048b
+// Secure Token: 271d7923-7bbf-44bf-a239-262fcd89048b
